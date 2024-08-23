f206b543f5e780655b69e096a62220e8ce6c923e
Matheus Motta <matheusdomingues423@gmail.com>
2024-08-23 11:56:59 -0300

    auth middleware

---
+++ src/middlewares/private-route-middleware.tsx
@@ -0,0 +1,34 @@
+"use client"
+import { useAuthContext } from "@/contexts/auth-context"
+import { Loader2 } from "lucide-react"
+import { useRouter } from "next/navigation"
+import React, { useEffect } from "react"
+
+const PrivateRouteMiddleware = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter()
+  const { user, isUserLoading } = useAuthContext()
+
+  useEffect(() => {
+    if (!isUserLoading) {
+      if (!user) {
+        router.push("/auth/login")
+      }
+    }
+  }, [user, router, isUserLoading])
+
+  if (isUserLoading) {
+    return (
+      <main className="flex min-h-screen flex-col items-center bg-zinc-950 justify-center">
+        <Loader2 className="size-20 animate-spin text-zinc-50" />
+      </main>
+    )
+  }
+
+  return (
+    <>
+      {children}
+    </>
+  )
+}
+
+export default PrivateRouteMiddleware
