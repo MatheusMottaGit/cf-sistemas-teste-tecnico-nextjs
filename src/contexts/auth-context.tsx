38688950775231fbeed42b64e91f3ba0545e5803
Matheus Motta <matheusdomingues423@gmail.com>
2024-08-23 11:56:11 -0300

    set auth context

---
+++ src/contexts/auth-context.tsx
@@ -0,0 +1,41 @@
+"use client"
+import { app } from "@/firebase/config/firebase"
+import { getAuth, onAuthStateChanged, User } from "firebase/auth"
+import React, { createContext, useContext, useEffect, useState } from "react"
+
+interface IAuthContext {
+  user: User | null
+  setUser: (user: User | null) => void
+  isUserLoading: boolean
+}
+
+export const AuthContext = createContext({} as IAuthContext)
+
+export const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null)
+  const [isUserLoading, setIsUserLoading] = useState(true)
+
+  const auth = getAuth(app)
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) =>{
+      if (user) {
+        setUser(user)
+      }else{
+        setUser(null)
+      }
+
+      setIsUserLoading(false)
+    })
+
+    return () => unsubscribe()
+  }, [auth])
+
+  return (
+    <AuthContext.Provider value={{ user, setUser, isUserLoading }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export const useAuthContext = () => useContext(AuthContext)
