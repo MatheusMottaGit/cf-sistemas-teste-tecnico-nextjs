4e77bff39afb53df9f3d681e6445ccca61247156
Matheus Motta <matheusdomingues423@gmail.com>
2024-08-25 23:24:49 -0300

    realtime database for production

--- src/firebase/config/firebase.ts
+++ src/firebase/config/firebase.ts
@@ -8,7 +8,8 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET || "",
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID || "",
   appId: process.env.NEXT_PUBLIC_APP_ID || "",
-  measurementId: process.env.NEXT_PUBLIC_MEASURAMENT_ID || ""
+  measurementId: process.env.NEXT_PUBLIC_MEASURAMENT_ID || "",
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL || ""
 }
 
 const app = initializeApp(firebaseConfig)
